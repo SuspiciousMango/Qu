30066483a041e73842be9d9518d7fb3bf6d55317
SuspiciousMango <jymangis@gmail.com>
2020-04-17 13:03:10 -0400

    Started on a login method

--- app/src/main/java/com/team5/qu/MainActivity.java
+++ app/src/main/java/com/team5/qu/MainActivity.java
@@ -3,6 +3,10 @@ package com.team5.qu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Sign In method created by Jayson
+     * Called when the user clicks the sign-in button
+     */
+    public void login(View v) {
+        EditText username = (EditText)(findViewById(R.id.username));
+        EditText password = (EditText)(findViewById(R.id.password));
+        Snackbar usernameError = Snackbar.make(v, "Username cannot be left blank", Snackbar.LENGTH_LONG);
+        Snackbar passwordError = Snackbar.make(v, "Password cannot be left blank", Snackbar.LENGTH_LONG);
+        Snackbar loginError = Snackbar.make(v, "Incorrect username or password", Snackbar.LENGTH_LONG);
+        //check if username and password fields are empty
+        if(username.getText().length() == 0) {
+            usernameError.show();
+        }
+        else if(password.getText().length() == 0) {
+            passwordError.show();
+        }
+        //check if username and password match an existing account
+        //log user in if so, display error otherwise
+        else {
+            loginError.show();
+        }
     }
 }
